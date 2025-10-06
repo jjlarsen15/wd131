1ea1b16631ff4278509331dbb91dbc63291f0ef3
jjlarsen15 <jjlarsen15@byui.edu>
2025-10-06 13:09:40 -0600

    ponder1

---
+++ test.js
@@ -0,0 +1,13 @@
+let day = "Monday";
+
+switch(day) {
+    case "Monday":
+        console.log("Start of the workweek!");
+        break
+    case "Friday":
+        console.log("Welcome to the weekend!");
+        break
+    case "Sunday":
+        console.log("Hope you got your church clothes on!");
+        break
+}
